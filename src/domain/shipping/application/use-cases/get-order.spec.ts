925b76f7df282bc50c031a665e468e922c278b7b
Marcos Parreiras <marcosalp1998@gmail.com>
2024-02-21 18:43:35 -0300

    feat: add get-order [use-case]

---
+++ src/domain/shipping/application/use-cases/get-order.spec.ts
@@ -0,0 +1,60 @@
+import { makeDeliveryMan } from "../../../../../test/factories/make-delivery-man";
+import { makeOrder } from "../../../../../test/factories/make-order";
+import { InMemoryDeliveryManRepository } from "../../../../../test/repositories/in-memory-delivery-man-repository";
+import { InMemoryOrderRepository } from "../../../../../test/repositories/in-memory-order-repository";
+import { NotAllowedError } from "../errors/not-allowed-error";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
+import { GetOrderUseCase } from "./get-order";
+
+describe("GetOrderUseCase [Use-Case]", () => {
+  let inMemoryDeliveryManRepository: InMemoryDeliveryManRepository;
+  let inMemoryOrderRepository: InMemoryOrderRepository;
+  let sut: GetOrderUseCase;
+
+  beforeEach(() => {
+    inMemoryDeliveryManRepository = new InMemoryDeliveryManRepository();
+    inMemoryOrderRepository = new InMemoryOrderRepository();
+    sut = new GetOrderUseCase(
+      inMemoryDeliveryManRepository,
+      inMemoryOrderRepository
+    );
+  });
+
+  it("Should be able to get an order", async () => {
+    const admin = makeDeliveryMan({ isAdmin: true });
+    const order = makeOrder();
+    inMemoryDeliveryManRepository.items.push(admin);
+    inMemoryOrderRepository.items.push(order);
+
+    const response = await sut.execute({
+      adminId: admin.id.toString(),
+      orderId: order.id.toString(),
+    });
+
+    expect(response.order).toBeTruthy();
+  });
+
+  it("Should not be able to get an order without admin", async () => {
+    const order = makeOrder();
+    inMemoryOrderRepository.items.push(order);
+
+    await expect(() => {
+      return sut.execute({
+        adminId: "",
+        orderId: order.id.toString(),
+      });
+    }).rejects.toBeInstanceOf(NotAllowedError);
+  });
+
+  it("Should not be able to get an unexistent order", async () => {
+    const admin = makeDeliveryMan({ isAdmin: true });
+    inMemoryDeliveryManRepository.items.push(admin);
+
+    await expect(() => {
+      return sut.execute({
+        adminId: admin.id.toString(),
+        orderId: "",
+      });
+    }).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
---
+++ src/domain/shipping/application/use-cases/get-order.ts
@@ -0,0 +1,38 @@
+import { Order } from "../../enterprise/entities/order";
+import { NotAllowedError } from "../errors/not-allowed-error";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
+import { DeliveryManRepository } from "../repositories/delivery-man-repository";
+import { OrderRepository } from "../repositories/order-repository";
+
+interface GetOrderUseCaseRequest {
+  adminId: string;
+  orderId: string;
+}
+
+interface GetOrderUseCaseResponse {
+  order: Order;
+}
+
+export class GetOrderUseCase {
+  constructor(
+    private deliveryManRepository: DeliveryManRepository,
+    private orderRepository: OrderRepository
+  ) {}
+
+  async execute({
+    adminId,
+    orderId,
+  }: GetOrderUseCaseRequest): Promise<GetOrderUseCaseResponse> {
+    const admin = await this.deliveryManRepository.findById(adminId);
+    if (!admin || !admin.isAdmin) {
+      throw new NotAllowedError();
+    }
+
+    const order = await this.orderRepository.findById(orderId);
+    if (!order) {
+      throw new ResourceNotFoundError();
+    }
+
+    return { order };
+  }
+}
