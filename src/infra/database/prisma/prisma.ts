93621bea17d07d89858b1f9bb3be1cafa25c5d27
Marcos Parreiras <marcosalp1998@gmail.com>
2024-03-01 11:17:22 -0300

    refactor: remove prisma initialization dependence of zod validation

--- src/infra/database/prisma/prisma.ts
+++ src/infra/database/prisma/prisma.ts
@@ -1,18 +1,17 @@
 import { PrismaClient } from "@prisma/client";
-import { env } from "../../env";
 import { randomUUID } from "crypto";
 
 function generateUniqueDataBaseUrl(schemaId: string) {
-  if (!env.DATABASE_URL) {
+  if (!process.env.DATABASE_URL) {
     throw new Error("Please provide a DATABASE_URL environment variable.");
   }
-  const url = new URL(env.DATABASE_URL);
+  const url = new URL(process.env.DATABASE_URL);
   url.searchParams.set("schema", schemaId);
   return url.toString();
 }
 
 export const databaseSchemaId = randomUUID();
-if (env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "test") {
   const databaseUrl = generateUniqueDataBaseUrl(databaseSchemaId);
   process.env.DATABASE_URL = databaseUrl;
 }
